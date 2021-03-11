72681b99aa1bb08b66fdded2f5ec24023818ed09
Ayoub mchichi <a.mchichi@laposte.net>
2021-03-11 09:58:00 +0100

    merge to main

---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '87850a6b552bb0625022e99101a478a78e88c8e7',
+    'name' => 'acs/oop',
+  ),
+  'versions' => 
+  array (
+    'acs/oop' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '87850a6b552bb0625022e99101a478a78e88c8e7',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '4a08cf4cdd2c38d12ee2b9fa69e5d235f37a6dcb',
+    ),
+  ),
+);
